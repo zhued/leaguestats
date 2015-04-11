a7e45059c2f1315cdf680737cd38f7075dd7448f
joshfermin <joshfermin@gmail.com>
2015-04-10 18:19:38 -0600

    mongo stuff

--- server/mongo.js
+++ server/mongo.js
@@ -1,13 +1,14 @@
-var mongoose    = require('mongoose');
+var mongoose = require('mongoose');
+require('dotenv').load();
 
-mongoose.connect('mongodb://Zandrr:password@ds047911.mongolab.com:47911/twitterstream');
+mongoose.connect(process.env.DB_CONNECT);
 
 
 var Any = new mongoose.Schema({ any: mongoose.Schema.Types.Mixed },{ strict: false });
-function tweetInit(db){
-  return mongoose.model('Tweet', Any, db);
+function dataInit(db){
+  return mongoose.model('Data', Any, db);
 }
 
 
-exports.tweetInit = tweetInit;
+exports.dataInit = dataInit;
 
