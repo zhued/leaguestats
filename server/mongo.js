92a0aea0d79c5b0b3a7508be7f23e34aeb3546ca
Edward Zhu <edward.r.zhu@gmail.com>
2015-04-13 15:01:31 -0600

    stream_data works with players, also implemented a DB close function

--- server/mongo.js
+++ server/mongo.js
@@ -9,6 +9,10 @@ function dataInit(db){
   return mongoose.model('Data', Any, db);
 }
 
+function DB_close(db){
+	return mongoose.disconnect();
+}
 
 exports.dataInit = dataInit;
+exports.DB_close = DB_close;
 
--- server/stream_data.js
+++ server/stream_data.js
@@ -4,14 +4,11 @@ var dotenv   = require('dotenv'), // used for keys -> get from .env
   exec = require('child_process').exec;
 
 var PythonShell = require('python-shell');
-// var pyshell = new PythonShell('../api_requests/data.py');
-
-var Data = db.dataInit('test');
 
 var arguments = process.argv.slice(2);
 
-
-console.log(arguments[0])
+var Data = db.dataInit(arguments[0]);
+// var Data = db.dataInit('test');
 
 var options = {
   scriptPath: 'api_requests',
@@ -20,13 +17,16 @@ var options = {
  
 PythonShell.run('data.py', options, function (err, times) {
 	if (err) throw err;
-		data = JSON.parse(times);
-		for (var key in data) {
-		    var entry = data[key];
-		    Data.create(entry, function(err,doc){
+	data = JSON.parse(times);
+	for (var key in data) {
+	    var entry = data[key];
+	    Data.create(entry, function(err,doc){
 			// if(err) throw err;
-				console.log(entry);
-			})
-		}
-		
+			// not throwing error because data will overlap, which is fine
+		})
+	}
+	
+	setTimeout(function(){ console.log('Entry Finished.'); db.DB_close(); }, 500);
 });
+
+
