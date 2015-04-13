dc0728e213000bb3cd12f996df22e4acd988b608
Edward Zhu <edward.r.zhu@gmail.com>
2015-04-13 15:22:53 -0600

    changed name of server and started mass stream

---
+++ server/get_times.js
@@ -0,0 +1,37 @@
+var dotenv   = require('dotenv'), // used for keys -> get from .env
+  fs        = require('fs'),
+  db     = require('./mongo.js'),
+  exec = require('child_process').exec;
+
+var PythonShell = require('python-shell');
+
+var arguments = process.argv.slice(2);
+
+// Keyword mass will run a mass update on all existing players in database
+if (arguments == "mass") {
+	setTimeout(function(){ console.log('Entry Finished.'); db.DB_close(); }, 500);
+} else { // Otherwise, just update the player given
+
+	var Data = db.dataInit(arguments[0]);
+	// var Data = db.dataInit('test');
+
+	var options = {
+	  scriptPath: 'api_requests',
+	  args: [arguments[0], 'get_recent_games']
+	};
+	 
+	PythonShell.run('data.py', options, function (err, times) {
+		if (err) throw err;
+		data = JSON.parse(times);
+		for (var key in data) {
+		    var entry = data[key];
+		    Data.create(entry, function(err,doc){
+				// if(err) throw err;
+				// not throwing error because data will overlap, which is fine
+			})
+		}
+		
+		setTimeout(function(){ console.log('Entry Finished.'); db.DB_close(); }, 500);
+	});
+}
+
