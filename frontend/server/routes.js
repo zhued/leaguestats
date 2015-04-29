d9d46c2c76ba0917b6a89930a3583fc443af5f3e
ridler <forrest.ridler@colorado.edu>
2015-04-29 16:28:50 -0600

    major layout/ui update

--- frontend/server/routes.js
+++ frontend/server/routes.js
@@ -14,12 +14,15 @@ module.exports = function(app){
 // ***
 // Gets times for a certain summoner
 // ***
+var cachedGames = [];
+
   app.get('/summoner/:summoner_name', function(req,res){
   	summoners.find({ summoner_short:req.params.summoner_name }, function(err,doc){
   		if(err) res.send(err);
       doc = JSON.stringify(doc);
       doc = JSON.parse(doc);
       games.find({ summoner_id:doc[0].summoner_id }, function(err,games){
+        cachedGames = games;
         if(err) res.send(err);
         res.send(games);
       });
@@ -27,21 +30,12 @@ module.exports = function(app){
   });
 
   app.get('/timedata/:summoner_name', function(req, res) {
-    summoners.find({ summoner_short:req.params.summoner_name }, function(err,doc){
-  		if(err) res.send(err);
-      doc = JSON.stringify(doc);
-      doc = JSON.parse(doc);
-      games.find({ summoner_id:doc[0].summoner_id }, function(err,games){
-        if(err) { res.send(err); } else {
-          var times = timeParser.parse(JSON.stringify(games));
-          var tsv = 'day\thour\tvalue\n';
-          for(var timeCode in times) {
-            tsv += times[timeCode].day+'\t'+times[timeCode].hour+'\t'+times[timeCode].value+'\n';
-          }
-          res.format({'text/plain': function() { res.send(tsv); }});
-        }
-      });
-  	});
+    var times = timeParser.parse(JSON.stringify(cachedGames));
+    var tsv = 'day\thour\tvalue\n';
+    for(var timeCode in times) {
+      tsv += times[timeCode].day+'\t'+times[timeCode].hour+'\t'+times[timeCode].value+'\n';
+    }
+    res.format({'text/plain': function() { res.send(tsv); }});
   });
 
 };
